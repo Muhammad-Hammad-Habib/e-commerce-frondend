2ac78a060bdcae4235efd5bb70d3344d69e2ab97
Muhammad Hammad Habib <muhammadhammad9946@gmail.com>
2025-11-17 22:07:42 +0500

    added landing page route

--- src/router.jsx
+++ src/router.jsx
@@ -1,8 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import LandingPage from "./layout/landingPage";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    children: [
+      {
+        path: "",
+        element: <SignIn />,
+      },
+    ],
+  },
+
   {
     path: "/signin",
     element: <SignIn />,
