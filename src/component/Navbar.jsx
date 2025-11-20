46e909b0aba3668fcf6b7363021cde6cf346d82d
Muhammad Hammad Habib <muhammadhammad9946@gmail.com>
2025-11-20 20:42:42 +0500

    navigation update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,6 +7,7 @@ import {
   MenuItem,
   MenuItems,
 } from "@headlessui/react";
+import logo from "../asset/logo.png";
 // import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import {
   Bars3Icon,
@@ -14,19 +15,26 @@ import {
   XMarkIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
-const navigation = [
-  { name: "Home", href: "#", current: true },
-  { name: "Shop", href: "#", current: false },
-  { name: "About", href: "#", current: false },
-  { name: "Contact", href: "#", current: false },
-];
+const Navbar = () => {
+  const [navigation, setNavigation] = useState([
+    { name: "Home", href: "#", current: true },
+    { name: "Shop", href: "#", current: false },
+    { name: "About", href: "#", current: false },
+    { name: "Contact", href: "signup", current: false },
+  ]);
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+  const classNames = (...classes) => {
+    return classes.filter(Boolean).join(" ");
+  };
 
-function Navbar() {
+  const handleOnClickActive = (index) => {
+    setNavigation((prev) =>
+      prev.map((item, i) => ({ ...item, current: i === index }))
+    );
+  };
+
   return (
     <Disclosure
       as="nav"
@@ -36,7 +44,7 @@ function Navbar() {
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+            <DisclosureButton className="custom-button group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon
@@ -51,18 +59,15 @@ function Navbar() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
             <div className="flex shrink-0 items-center">
-              <img
-                alt="Your Company"
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                className="h-8 w-auto"
-              />
+              <img alt="Your Company" src={logo} className="h-10 w-auto" />
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                {navigation.map((item) => (
+                {navigation.map((item, key) => (
                   <a
                     key={item.name}
                     href={item.href}
+                    onClick={() => handleOnClickActive(key)}
                     aria-current={item.current ? "page" : undefined}
                     className={classNames(
                       item.current
@@ -144,26 +149,34 @@ function Navbar() {
 
       <DisclosurePanel className="sm:hidden">
         <div className="space-y-1 px-2 pt-2 pb-3">
-          {navigation.map((item) => (
+          {navigation.map((item, key) => (
             <DisclosureButton
               key={item.name}
               as="a"
+              onClick={() => handleOnClickActive(key)}
               href={item.href}
               aria-current={item.current ? "page" : undefined}
               className={classNames(
                 item.current
-                  ? "bg-gray-950/50 text-white"
-                  : "text-gray-300 hover:bg-white/5 hover:text-white",
+                  ? "bg-gradient-to-r from-[#fd8e36] to-[#fbbf77] text-white"
+                  : "text-[#ce6c45] hover:bg-white/5 hover:text-[#f4713d]",
                 "block rounded-md px-3 py-2 text-base font-medium"
               )}
             >
               {item.name}
+
+              {/* className={classNames(
+                      item.current
+                        ? "bg-gradient-to-r from-[#fd8e36] to-[#fbbf77] text-white"
+                        : "text-[#ce6c45] hover:bg-white/5 hover:text-[#f4713d]",
+                      "rounded-md px-3 py-2 text-sm font-medium"
+                    )} */}
             </DisclosureButton>
           ))}
         </div>
       </DisclosurePanel>
     </Disclosure>
   );
-}
+};
 
 export default Navbar;
