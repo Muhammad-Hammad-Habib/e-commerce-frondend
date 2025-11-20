c251c738bd80ba919cfd1f16b79a21299b279901
Muhammad Hammad Habib <muhammadhammad9946@gmail.com>
2025-11-20 20:43:05 +0500

    added new section

--- src/layout/LandingPage.jsx
+++ src/layout/LandingPage.jsx
@@ -4,15 +4,19 @@ import HeroSection from "../component/HeroSection";
 import FeaturedProducts from "../component/FeaturedProducts";
 import WhyChooseUs from "../component/WhyChooseUs";
 import BrowseCategories from "../component/BrowseCategories";
+import AboutUs from "../component/AboutUs";
+import Footer from "../component/Footer";
 
 const LandingPage = () => {
   return (
     <div className="container-lg bg-[#FFF6E5] ">
       <Navbar />
       <HeroSection />
-      <FeaturedProducts/>
-      <WhyChooseUs/>
-      <BrowseCategories/>
+      <FeaturedProducts />
+      <WhyChooseUs />
+      <BrowseCategories />
+      <AboutUs />
+      <Footer />
     </div>
   );
 };
