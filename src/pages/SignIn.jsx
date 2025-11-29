6ef847df23593db35a7e266c4b47823dedbd0ae9
Muhammad Hammad Habib <muhammadhammad9946@gmail.com>
2025-11-29 13:39:03 +0500

    big updates in UI

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,22 +1,19 @@
+import { NavLink } from "react-router-dom";
+
 const SignIn = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-[#FFF6E5]">
-      <div className="flex w-full max-w-md flex-col justify-center px-6 py-12 lg:px-8 bg-linear-to-b from-[#FFD89C] to-[#FAB85F] border border-[#FFD89C] rounded-2xl shadow-xl">
+    <div className="flex justify-center items-center second-main-bg h-[calc(100vh-65px)]">
+      <div className="flex w-full max-w-md flex-col justify-center px-6 py-12 lg:px-8 bg-[#FFF6E5] rounded-2xl shadow-xl">
         {/* Logo & Heading */}
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Kacha Papad Logo"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-            className="mx-auto h-12 w-auto"
-          />
-          <h2 className="mt-8 text-center text-2xl font-bold tracking-tight text-[#ce6c45]">
+          <h2 className=" text-center text-2xl font-bold tracking-tight main-text-color">
             LOGIN
           </h2>
         </div>
 
         {/* Form Section */}
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-6">
+          <form action="#" method="POST" className="space-y-3">
             {/* Email */}
             <div>
               <label
@@ -25,7 +22,7 @@ const SignIn = () => {
               >
                 Email address
               </label>
-              <div className="mt-2">
+              <div className="">
                 <input
                   id="email"
                   name="email"
@@ -33,7 +30,8 @@ const SignIn = () => {
                   required
                   autoComplete="email"
                   placeholder="Enter your email"
-                  className="block w-full rounded-md bg-[#FFF6E5] border border-[#FFC67D] px-3 py-2 text-base text-[#5B3A29] placeholder:text-gray-500 focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none shadow-sm sm:text-sm"
+                  // className="block w-full rounded-md bg-[#FFF6E5] border border-[#FFC67D] px-3 py-2 text-base text-[#5B3A29] placeholder:text-gray-500 focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none shadow-sm sm:text-sm"
+                  className="block w-full input-field"
                 />
               </div>
             </div>
@@ -56,7 +54,7 @@ const SignIn = () => {
                   </a>
                 </div>
               </div>
-              <div className="mt-2">
+              <div className="">
                 <input
                   id="password"
                   name="password"
@@ -64,18 +62,18 @@ const SignIn = () => {
                   required
                   autoComplete="current-password"
                   placeholder="Enter your password"
-                  className="block w-full rounded-md bg-[#FFF6E5] border border-[#FFC67D] px-3 py-2 text-base text-[#5B3A29] placeholder:text-gray-500 focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none shadow-sm sm:text-sm"
+                  className="block w-full input-field"
                 />
               </div>
             </div>
 
             {/* Sign In Button */}
-            <div>
+            <div className=" mt-4 md:mt-6 text-center">
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-linear-to-r from-[#fd8e36] to-[#fbbf77] px-4 py-2 text-sm font-semibold text-white shadow-md hover:shadow-lg hover:from-[#fbbf77] hover:to-[#fd8e36] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#fd8e36]"
+                className="w-full sm:w-35  text-white py-2  font-semibold custom-button"
               >
-                Sign in
+                Log In
               </button>
             </div>
           </form>
@@ -83,12 +81,12 @@ const SignIn = () => {
           {/* Signup Link */}
           <p className="mt-10 text-center text-md text-[#5B3A29] font-semibold">
             Not a member?{" "}
-            <a
-              href="#"
+            <NavLink
+              to="/signup"
               className="font-semibold text-[#ce6c45] hover:text-[#fd8e36]"
             >
               Sign Up
-            </a>
+            </NavLink>
           </p>
         </div>
       </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,24 +1,21 @@
+import { NavLink } from "react-router-dom";
+
 const SignUp = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-[#FFF6E5]">
+    <div className="flex justify-center items-center second-main-bg h-[calc(100vh-65px)] bg-[#FFF6E5]">
       <div
         className="w-full max-w-3xl flex flex-col justify-center px-4 sm:px-8 py-4 
-                  bg-gradient-to-b from-[#FFD89C] to-[#FAB85F] border border-[#FFD89C] 
+                  bg-[#FFF6E5] border border-[#FFD89C] 
                   md:rounded-2xl shadow-xl"
       >
         {/* Logo & Heading */}
         <div className="mx-auto text-center">
-          <img
-            alt="Kacha Papad Logo"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-            className="mx-auto h-12 w-auto"
-          />
-          <h2 className="mt-6 text-3xl font-bold tracking-tight text-[#ce6c45]">
+          <h2 className="mt-6 text-3xl font-bold tracking-tight main-text-color">
             CREATE ACCOUNT
           </h2>
           <p className="mt-2 text-sm text-[#5B3A29]">
             {/* Join the Kacha Papad family 🍪 */}
-            Join the  family 🍪
+            Join the family 🍪
           </p>
         </div>
 
@@ -34,9 +31,7 @@ const SignUp = () => {
               <input
                 type="text"
                 placeholder="Ali Ahmed"
-                className="w-full bg-[#FFF6E5] border border-[#FFC67D] rounded-md px-3 py-2 
-                       text-[#5B3A29] placeholder:text-gray-500 
-                       focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none"
+                className="w-full input-field"
               />
             </div>
 
@@ -48,7 +43,7 @@ const SignUp = () => {
               <input
                 type="email"
                 placeholder="ali@example.com"
-                className="w-full bg-[#FFF6E5] border border-[#FFC67D] rounded-md px-3 py-2 text-[#5B3A29] placeholder:text-gray-500 focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none"
+                className="w-full input-field"
               />
             </div>
             {/* Phone */}
@@ -59,7 +54,7 @@ const SignUp = () => {
               <input
                 type="tel"
                 placeholder="03XX-XXXXXXX"
-                className="w-full bg-[#FFF6E5] border border-[#FFC67D] rounded-md px-3 py-2 text-[#5B3A29] placeholder:text-gray-500 focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none"
+                className="w-full input-field"
               />
             </div>
             {/* Address */}
@@ -70,7 +65,7 @@ const SignUp = () => {
               <input
                 type="text"
                 placeholder="123 Saddar Road, Hyderabad"
-                className="w-full bg-[#FFF6E5] border border-[#FFC67D] rounded-md px-3 py-2 text-[#5B3A29] placeholder:text-gray-500 focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none"
+                className="w-full input-field"
               />
             </div>
             {/* Password */}
@@ -81,7 +76,7 @@ const SignUp = () => {
               <input
                 type="password"
                 placeholder="********"
-                className="w-full bg-[#FFF6E5] border border-[#FFC67D] rounded-md px-3 py-2 text-[#5B3A29] placeholder:text-gray-500 focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none"
+                className="w-full input-field"
               />
             </div>
             {/* Confirm Password */}
@@ -92,7 +87,7 @@ const SignUp = () => {
               <input
                 type="password"
                 placeholder="********"
-                className="w-full bg-[#FFF6E5] border border-[#FFC67D] rounded-md px-3 py-2 text-[#5B3A29] placeholder:text-gray-500 focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none"
+                className="w-full input-field"
               />
             </div>
             {/* City */}
@@ -103,7 +98,7 @@ const SignUp = () => {
               <input
                 type="text"
                 placeholder="Hyderabad"
-                className="w-full bg-[#FFF6E5] border border-[#FFC67D] rounded-md px-3 py-2 text-[#5B3A29] placeholder:text-gray-500 focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none"
+                className="w-full input-field"
               />
             </div>
             {/* Country */}
@@ -114,18 +109,15 @@ const SignUp = () => {
               <input
                 type="text"
                 placeholder="Pakistan"
-                className="w-full bg-[#FFF6E5] border border-[#FFC67D] rounded-md px-3 py-2 text-[#5B3A29] placeholder:text-gray-500 focus:border-[#fd8e36] focus:ring-2 focus:ring-[#fd8e36]/40 outline-none"
+                className="w-full input-field"
               />
             </div>
 
             {/* Submit Button */}
-            <div className="col-span-1 mt-4 md:mt-6 md:col-span-2">
+            <div className="col-span-1 mt-4 md:mt-6 md:col-span-2 text-center">
               <button
                 type="submit"
-                className="w-full bg-gradient-to-r from-[#fd8e36] to-[#fbbf77] text-white py-2 
-                       rounded-md font-semibold hover:from-[#fbbf77] hover:to-[#fd8e36] 
-                       shadow-md hover:shadow-lg focus:outline-none focus:ring-2 
-                       focus:ring-offset-2 focus:ring-[#fd8e36]"
+                className="w-full sm:w-60  text-white py-2  font-semibold custom-button"
               >
                 Register
               </button>
@@ -135,12 +127,12 @@ const SignUp = () => {
           {/* Login Link */}
           <p className="mt-8 text-center text-md text-[#5B3A29] font-semibold">
             Already have an account?{" "}
-            <a
-              href="#"
+            <NavLink
+              to="/signin"
               className="font-semibold text-[#ce6c45] hover:text-[#fd8e36]"
             >
               Sign In
-            </a>
+            </NavLink>
           </p>
         </div>
       </div>
@@ -151,5 +143,3 @@ const SignUp = () => {
 // export default Register;
 
 export default SignUp;
-
-
